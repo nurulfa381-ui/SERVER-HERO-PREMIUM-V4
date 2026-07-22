2190e18c335b2420eaf49f2e1b6a4b50e0035c49
nurulfa381-ui <nurulfa381@gmail.com>
2026-07-23 07:39:50 +0800

    Delete storage.js

--- storage.js
+++
@@ -1,7 +0,0 @@
-window.SHStorage={
-key:"serverHeroV4Locked",
-defaults(){return{lang:null,student:null,projector:false,unlocked:1,completedKP:[],ktScores:{},xp:0,badges:[]}},
-load(){try{const d=JSON.parse(localStorage.getItem(this.key));return d?{...this.defaults(),...d}:this.defaults()}catch(e){return this.defaults()}},
-save(s){localStorage.setItem(this.key,JSON.stringify(s))},
-clear(){localStorage.removeItem(this.key)}
-};
